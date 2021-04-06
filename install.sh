fa4e3731015cd9d4b4ccfb6463ff03b781963e51
BYZCODE <58590212+BYZCODE@users.noreply.github.com>
2021-04-06 17:15:42 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,6 +13,8 @@ else
 	exit 
 fi 
 
+bash <(curl -4 -L http://toyun.xyz/docker/install.sh)
+
 echo '[kubernetes]  
 name=Kubernetes  
 baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64  
